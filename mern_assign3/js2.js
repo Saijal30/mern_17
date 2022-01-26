22f03dac606f7fe8adafd289743e79a49637560a
Saijal30 <70759081+Saijal30@users.noreply.github.com>
2022-01-26 18:13:04 +0530

    Create js2.js

---
+++ mern_assign3/js2.js
@@ -0,0 +1,16 @@
+var num1=10;
+var num2=20;
+var num3=30;
+if(num1>num2){
+  if(num1>num3){
+    console.log(num1 + " is greatest number");}
+  else if(num1<num3){
+    console.log(num3 + "is greatest number");}
+}
+else if(num1<num2){
+  if(num2>num3){
+    console.log(num2 + "is greatest number");}
+  else if(num2<num3){
+    console.log(num3 + " is greatest number");}
+}
+  
