fd3ad1a86ca7f355fe1a6b3c8a87db94be9ff6d5
Saijal30 <70759081+Saijal30@users.noreply.github.com>
2022-02-06 18:31:50 +0530

    Create Q2.js

---
+++ jan26/Q2/Q2.js
@@ -0,0 +1,7 @@
+var char = prompt("Enter a character:");
+if(char=="a" || char=="A" || char=="i" ||char=="I"|| char=="e"|| char=="E"||char=="o" ||char=="O" ||char=="u" ||char=="U"){
+    alert(char + " is a vowel.");
+}
+else{
+    alert(char+" is a consonant");
+}
